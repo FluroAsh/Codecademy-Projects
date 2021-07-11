74c7aebf267b357419f2dc23bd5b5f55bd4c4fa3
Ashley Thompson <orangeash@hotmail.com.au>
2021-07-11 23:10:37 +1000

    Update Carly's Clippers.py

--- Carly's Clippers.py
+++ Carly's Clippers.py
@@ -12,3 +12,64 @@ Each index in hairstyles corresponds to an associated index in prices and last_w
 
 For example, The hairstyle "bouffant" has an associated price of 30 from the prices list, and was purchased 2 times in the last week as shown in the last_week list. Each of these elements are in the first index of their respective lists.
 '''
+
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+# Calculate sum for indicies in prices
+total_price = 0
+for i in prices:
+  total_price += i
+print(total_price)
+
+# Divide total price by number of indicies to create average_price
+average_price = total_price / len(prices)
+print("Average Haircut Price: ${0}".format(average_price))
+
+# Price is too expensive... cut all prices by 5 dollars!
+new_prices = [
+  i - 5 for i in prices
+]
+
+print(new_prices)
+
+''' 
+** ALTERNATIVE TO ABOVE **
+new_prices = []
+for i in prices:
+  new_prices.append(i - 5)
+
+print(new_prices)
+'''
+
+# How much total revenue was brought in last week?
+print("\n")
+total_revenue = 0
+
+for i in range(len(hairstyles)):
+  total_revenue += prices[i] * last_week[i]
+print("Total Revenue: ${0}".format(total_revenue))
+
+# Divide by 7 to find average daily revenue
+average_daily_revenue = total_revenue / 7
+print("Average Daily Revenue: ${0}".format(average_daily_revenue))
+
+cuts_under_30 = [
+  hairstyles[i]
+  for i in range(len(hairstyles))
+  if new_prices[i] < 30
+]
+
+print(cuts_under_30)
+
+'''
+** ALTERNATIVE TO ABOVE **
+cuts_under_30 = []
+for i in range(len(hairstyles)):
+  if new_prices[i] < 30:
+    cuts_under_30.append(hairstyles[i])
+    
+print(cuts_under_30)
+'''
