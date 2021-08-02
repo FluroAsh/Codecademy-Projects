53eee2ad89891e4f1c746346d9d2a082437136ce
Ashley Thompson <orangeash@hotmail.com.au>
2021-08-02 19:57:25 +1000

    Update getting_ready_for_physics_class.py

--- getting_ready_for_physics_class.py
+++ getting_ready_for_physics_class.py
@@ -12,23 +12,21 @@ def f_to_c(f_temp):
 f100_in_celsius = f_to_c(100)
 
 def c_to_f(c_temp):
-  f_temp = c_temp * (9/5) + 32
+  f_temp = (c_temp * 9/5) + 32
   return f_temp
 
 c0_in_fahrenheit = c_to_f(0)
 
 # Function to find the train force determined by its mass * acceleration
-def get_force(train_mass, train_acceleration):
-  train_force = train_mass * train_acceleration
-  return train_force
+def get_force(mass, acceleration):
+  return mass * acceleration
 
 train_force = get_force(train_mass, train_acceleration)
 print("The GE train supplies " + str(train_force) + " Newtons of force.")
 
 # Function to find the amount of energy emitted by a bomb based on its mass in kg
 def get_energy(mass, c = 3*10**8):
-  bomb_energy = mass * c**2
-  return bomb_energy
+  return mass * c**2
 
 bomb_energy = get_energy(bomb_mass)
 
@@ -36,8 +34,7 @@ print("A 1kg bomb supplies " + str(bomb_energy) + " Joules")
 
 # Function to find the amount of work (in joules) that the train puts out over 'x' distance
 def get_work(mass, acceleration, distance):
-  work = get_force(mass, acceleration)*distance
-  return work
+  return get_force(mass, acceleration)*distance
 
 train_work = get_work(train_mass, train_acceleration, train_distance)
 
